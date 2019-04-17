fbb20066ff7655e2c365ff9bf5281da68a8946a2
jh-RLI <jonas.huber@rl-institut.de>
2019-04-17 15:18:26 +0200

    added sqlalchemy engine

--- app_settings.py
+++ app_settings.py
@@ -1,1 +1,20 @@
 #create sqlalchemy con
+
+import os
+import sqlahelper
+from sqlalchemy import create_engine
+from configobj import ConfigObj
+
+from wam import settings
+from . import oep_models
+
+
+wam_config = settings.config['DATABASES']['OEP']
+
+############################################SQLAlchemy ENGINE#####+#####################################################
+DB_URL = '{ENGINE}://{USER}:{PASSWORD}@{HOST}:{PORT}'.format(**wam_config)
+# DB_URL = '{}://{}:{}@{}:{}'.formt()
+
+engine = create_engine(DB_URL)
+sqlahelper.add_engine(engine)
+########################################################################################################################
