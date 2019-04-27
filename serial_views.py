b442351ebb8de60ae0dd41e5e370d5d826128f01
jh-RLI <jonas.huber@rl-institut.de>
2019-04-27 18:57:25 +0200

    add custom serializer, use geojson and geoalchemy2 and shapely, connect with sqlahelper engine, why cant import geom see ToDo

--- serial_views.py
+++ serial_views.py
@@ -1,3 +1,27 @@
-from .models import *
-from djgeojson.views import GeoJSONLayerView
+# serialize data for all models
 
+import WAM_APP_FRED.oep_models as oep_modles
+# from WAM_APP_FRED.db_sqla import *
+from shapely.wkb import loads
+from geojson import Feature, FeatureCollection, dumps
+from geoalchemy2 import functions
+import sqlahelper as sah
+
+
+def serializer():
+    Session = sah.get_session()
+    session = Session()
+
+    features = []
+
+    for record in session.query(oep_modles.classes['Location']).limit(100):
+        # ToDo: why type error: TypeError: bytes or integer address expected instead of str instance
+        geometry = loads(record.point.wkb)
+        feature = Feature(id=record.id, geometry=geometry)
+        features.append(feature)
+
+    return dumps(FeatureCollection(features))
+
+
+print(serializer())
+print('WAIT')
