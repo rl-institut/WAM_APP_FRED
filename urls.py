00ac7759fa64019e3adce5d754730c3708b41d10
Jonas H <jonas.huber@rl-institut.de>
2019-04-29 15:19:33 +0200

    #3 provide testdataset from serializer as url

--- urls.py
+++ urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from WAM_APP_FRED.serial_views import serializer
 app_name = 'WAM_APP_FRED'
 
 urlpatterns = [
     # path('', views.fred_map, name='index')
-    path('', views.webgui_test, name='index')
+    path('', views.webgui_test, name='index'),
 
+    # 1 - Line
+    # path('Lines.data/', serial_views.LinesData.as_view(), name='Lines.data'),
+
+    path('Locations.data/', serializer(), name='Locations.data')
 ]
