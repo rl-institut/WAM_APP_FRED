8ead23c30b847ccdc61ad9927688c4ad02a5f0ae
Jonas H <jonas.huber@rl-institut.de>
2019-04-29 15:18:29 +0200

    #3 add docstring

--- serial_views.py
+++ serial_views.py
@@ -9,18 +9,30 @@ import sqlahelper as sah
 
 
 def serializer():
+    """
+    returns the queryed table id, geom as GEOJSON featureCollection
+
+    :return: dict - geojson featureCollection
+    """
+
+    #ToDO: after testing done change to input pram
     Session = sah.get_session()
     session = Session()
+    ##############################################
 
     features = []
 
     for record in session.query(oep_modles.classes['Location']).limit(100):
         geometry = loads(str(record.point), True)
+        propertys = record.id
         feature = Feature(id=record.id, geometry=geometry)
+
         features.append(feature)
 
     return dumps(FeatureCollection(features))
 
 
-print(serializer())
-print('WAIT')
+# print(serializer())
+# print('WAIT')
+
+
