ac1a7a8dd8a1b8baf377b33f72287bd4345b447e
jh-RLI <jonas.huber@rl-institut.de>
2019-05-21 15:34:50 +0200

    recreate serializer function as class, add class methodes that serialize simple geometry and propertys for each geometry, cleaned unnecessary functions

--- serial_views.py
+++ serial_views.py
@@ -1,6 +1,7 @@
 # serialize data for all models
 from django.http import HttpResponse
 
+
 import WAM_APP_FRED.oep_models as oep_modles
 # from WAM_APP_FRED.db_sqla import *
 from shapely.wkb import loads as loadswkb
@@ -32,7 +33,7 @@ from datetime import datetime
 #
 #     return dumps(FeatureCollection(features))
 
-def serializer():
+class Serializer():
     """
     returns a query result containing a full record from OEP table model_draft.openfred_series
     as GEOJSON featureCollection. All related tables are joined and the values are included as
@@ -46,52 +47,68 @@ def serializer():
     session = Session()
     ##############################################
 
-    features = []
+    def geometry_view(serlf):
+        features = []
 
-    for record in session.query(oep_modles.classes['Series'],
-                                oep_modles.classes['Timespan'],
-                                oep_modles.classes['Location'],
-                                oep_modles.classes['Variable']).join(oep_modles.classes['Timespan'])\
-                                                               .join(oep_modles.classes['Location'])\
-                                                               .join(oep_modles.classes['Variable']).limit(100):
+        for record in Serializer.session.query(oep_modles.classes['Series'],
+                                               oep_modles.classes['Location'])\
+                                                .join(oep_modles.classes['Location']).limit(100000):
 
-        # Collection all Columns to be included from tables timespan and values
-        # ToDo: maybe serialize the following on another session
-        # record.Series.timespan.segments not included "list to long"
-        # record.Series.values not included "list to long"
-        # ToDo: How to handel the DateTimeObj so it is Json Serializeable
-        # timespan_collection = {"Start": record.Series.timespan.start.strftime('%b %d %Y %I:%M%p'),
-        #                        "Stop": record.Series.timespan.stop.strftime('%b %d %Y %I:%M%p'),
-        #                        "Resolution": record.Series.timespan.resolution}
+            geometry = loadswkb(str(record.Series.location.point), True)
+            feature = Feature(id=record.Series.id, geometry=geometry)
+            features.append(feature)
 
-        netcdf = record.Series.variable.netcdf_attributes
-        variables_collection = {"Name": record.Series.variable.name}
-        variables_collection["NetCDF"] = netcdf
-
-        geometry = loadswkb(str(record.Series.location.point), True)
-
-        propertys = {"SeriesID": record.Series.id, "values": record.Series.values, "height": record.Series.height}
-        # propertys.update(timespan_collection)
-        propertys.update(variables_collection)
-        feature = Feature(id=record.Series.location_id, geometry=geometry, properties=propertys)
-
-        features.append(feature)
-
-    return dumps(FeatureCollection(features))
+        return HttpResponse(dumps(FeatureCollection(features)), content_type="application/json")
 
 
-class GeoView():
-    """
-    geo view object
-    """
 
-    def geojson_view(self):
-        """
-        :return: Geojson as http response for url
-        """
-        geojsondata = serializer()
-        # return render(geojsondata, GeoView.template_name, context={'geo_json':geojsondata})
-        return HttpResponse(geojsondata, content_type="application/json")
+    def property_view(self):
+        features = []
 
-    def geo_filter(self):
-        pass
+        for record in Serializer.session.query(oep_modles.classes['Series'],
+                                    oep_modles.classes['Timespan'],
+                                    oep_modles.classes['Variable']).join(oep_modles.classes['Timespan'])\
+                                                                   .join(oep_modles.classes['Variable']).limit(1000):
+
+            # Collection all Columns to be included from tables timespan and values
+            # ToDo: maybe serialize the following on another session
+            # record.Series.timespan.segments not included "list to long"
+            # record.Series.values not included "list to long"
+            # ToDo: How to handel the DateTimeObj so it is Json Serializeable
+            # timespan_collection = {"Start": record.Series.timespan.start.strftime('%b %d %Y %I:%M%p'),
+            #                        "Stop": record.Series.timespan.stop.strftime('%b %d %Y %I:%M%p'),
+            #                        "Resolution": record.Series.timespan.resolution}
+
+            netcdf = record.Series.variable.netcdf_attributes
+            variables_collection = {"Name": record.Series.variable.name}
+            variables_collection["NetCDF"] = netcdf
+
+
+            propertys = {"SeriesID": record.Series.id, "values": record.Series.values, "height": record.Series.height}
+            # propertys.update(timespan_collection)
+            propertys.update(variables_collection)
+            feature = Feature(id=record.Series.id, properties=propertys)
+            features.append(feature)
+
+        return HttpResponse(dumps(FeatureCollection(features)), content_type="application/json")
+
+        # def get_single_weatherpoint(self, request):
+        #     """
+        #     Return the data for the closest weather-point for a given position
+        #     as GeoJSON.
+        #
+        #     request: is the current mouse position passed from client
+        #     :return:
+        #     """
+        #     if request.method is 'POST':
+        #         position = request.POST.get('latlng')
+        #
+        #         for record in Serializer.session.query(oep_modles.classes['Series'],
+        #                                                oep_modles.classes['Timespan'],
+        #                                                oep_modles.classes['Variable'])\
+        #                                                 .join(oep_modles.classes['Timespan']) \
+        #                                                 .join(oep_modles.classes['Variable']).limit(1000):
+        #
+        #
+        #             return HttpResponse(geojsondata, content_type="application/json")
+
