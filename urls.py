6670b9235ae391b49ae726151b3b3ed0cee13d66
jh-RLI <jonas.huber@rl-institut.de>
2019-05-27 14:48:38 +0200

    added a url that contains weather point data as GEOJSON feature

--- urls.py
+++ urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.webgui_test, name='index'),
 
     # just point geometrys as geojson featureCollection
-    path('Locations.data/', Serializer.wseries_geometry_view, name='Locations.data')
+    path('Locations.data/', Serializer.wseries_geometry_view, name='Locations.data'),
+    path('MouseClick.data/', Serializer.wseries_get_single_point, name='MouseClick.data')
 ]
