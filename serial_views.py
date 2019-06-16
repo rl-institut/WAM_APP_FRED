96db427c47d0d3997292c984cf3b957921a3a3a7
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-06-17 00:00:54 +0200

    Add view for the user click on the map (get the position)

--- serial_views.py
+++ serial_views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .app_settings import LOCAL_TESTING
 
+
+
 if not LOCAL_TESTING:
     import WAM_APP_FRED.oep_models as oep_models
 # from WAM_APP_FRED.db_sqla import *
@@ -81,51 +83,53 @@ class Serializer():
 
 
 
-    def wseries_property_view(self):
-        """
-        returns a query result containing a full record from OEP table as GEOJSON featureCollection.
-        No geometry is included.
-        All related tables are joined and the values are included as property within the GEOJSON.
+    # def wseries_property_view(self):
+    #     """
+    #     returns a query result containing a full record from OEP table as GEOJSON featureCollection.
+    #     No geometry is included.
+    #     All related tables are joined and the values are included as property within the GEOJSON.
+    #
+    #
+    #     :return:
+    #     """
+    #
+    #     features = []
+    #
+    #     for record in Serializer.session.query(
+    #             oep_models.classes['Series'],
+    #             oep_models.classes['Timespan'],
+    #             oep_models.classes['Variable']
+    #     ).join(oep_models.classes['Timespan']).join(oep_models.classes['Variable']).limit(5):
+    #
+    #         # Collection all Columns to be included from tables timespan and values
+    #         # ToDo: maybe serialize the following on another session
+    #         # record.Series.timespan.segments not included "list to long"
+    #         # record.Series.values not included "list to long"
+    #         # ToDo: How to handel the DateTimeObj so it is Json Serializeable
+    #         # timespan_collection = {"Start": record.Series.timespan.start.strftime('%b %d %Y %I:%M%p'),
+    #         #                        "Stop": record.Series.timespan.stop.strftime('%b %d %Y %I:%M%p'),
+    #         #                        "Resolution": record.Series.timespan.resolution}
+    #
+    #         netcdf = record.Series.variable.netcdf_attributes
+    #         variables_collection = {"Name": record.Series.variable.name}
+    #         variables_collection["NetCDF"] = netcdf
+    #
+    #
+    #         propertys = {
+    #             "SeriesID": record.Series.id,
+    #             "values": record.Series.values,
+    #             "height": record.Series.height
+    #         }
+    #         # propertys.update(timespan_collection)
+    #         propertys.update(variables_collection)
+    #         feature = Feature(id=record.Series.id, properties=propertys)
+    #         features.append(feature)
+    #
+    #     return HttpResponse(dumps(FeatureCollection(features)), content_type="application/json")
 
 
-        :return:
-        """
 
-        features = []
-
-        for record in Serializer.session.query(
-                oep_models.classes['Series'],
-                oep_models.classes['Timespan'],
-                oep_models.classes['Variable']
-        ).join(oep_models.classes['Timespan']).join(oep_models.classes['Variable']).limit(5):
-
-            # Collection all Columns to be included from tables timespan and values
-            # ToDo: maybe serialize the following on another session
-            # record.Series.timespan.segments not included "list to long"
-            # record.Series.values not included "list to long"
-            # ToDo: How to handel the DateTimeObj so it is Json Serializeable
-            # timespan_collection = {"Start": record.Series.timespan.start.strftime('%b %d %Y %I:%M%p'),
-            #                        "Stop": record.Series.timespan.stop.strftime('%b %d %Y %I:%M%p'),
-            #                        "Resolution": record.Series.timespan.resolution}
-
-            netcdf = record.Series.variable.netcdf_attributes
-            variables_collection = {"Name": record.Series.variable.name}
-            variables_collection["NetCDF"] = netcdf
-
-
-            propertys = {
-                "SeriesID": record.Series.id,
-                "values": record.Series.values,
-                "height": record.Series.height
-            }
-            # propertys.update(timespan_collection)
-            propertys.update(variables_collection)
-            feature = Feature(id=record.Series.id, properties=propertys)
-            features.append(feature)
-
-        return HttpResponse(dumps(FeatureCollection(features)), content_type="application/json")
-
-    def wseries_get_single_point(self, request):
+    def wseries_get_single_point(request):
         """
         Return the data for the closest weather-point for a given position
         as GeoJSON.
@@ -135,9 +139,22 @@ class Serializer():
 
         :return: GeoJSON feature as HTTP response
         """
-        if request.method is 'POST':
-            position = request.POST.get('latlng')
-            print(position)
+
+        features = []
+        if request.method == 'POST':
+            print(request.POST)
+            lat = float(request.POST.get('lat'))
+            long = float(request.POST.get('long'))
+            print(lat, long)
+
+
+            geometry = Point((lat, long))
+            # geometry = loadswkb(str(record.Series.location.point), True)
+            # feature = Feature(id=record.Series.id, geometry=geometry)
+            feature = Feature(id=102, geometry=geometry)
+            features.append(feature)
+
+
             # for record in Serializer.session.query(
             #         oep_models.classes['Series'],
             #         oep_models.classes['Timespan'],
@@ -147,8 +164,10 @@ class Serializer():
             #     geom = ""
             #     propertys = ""
             #     feature = Feature(id= "", geometry=geom, property=propertys)
+        elif request.method == 'GET':
+            print(request.GET)
 
-        return HttpResponse('Success')
+        return HttpResponse(dumps(FeatureCollection(features)), content_type="application/json")
         # return HttpResponse(feature, content_type="application/json")
 
     def pp_list_geometry_view(self):
