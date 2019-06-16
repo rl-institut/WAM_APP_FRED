fcd42f42b13863601d03531997fb11b35cb526a5
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-06-16 23:45:42 +0200

    Update the view to display the weather stations on the map

--- urls.py
+++ urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from WAM_APP_FRED.serial_views import Serializer
+
+
+from djgeojson.views import GeoJSONLayerView
+
+from .models import WeatherStation
+
 app_name = 'WAM_APP_FRED'
 
 urlpatterns = [
@@ -9,5 +16,6 @@ urlpatterns = [
 
     # just point geometrys as geojson featureCollection
     path('Locations.data/', Serializer.wseries_geometry_view, name='Locations.data'),
-    path('MouseClick.data/', Serializer.wseries_get_single_point, name='MouseClick.data')
+    path('MouseClick.data/', Serializer.wseries_get_single_point, name='MouseClick.data'),
+    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=WeatherStation), name='data')
 ]
