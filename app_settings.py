7f6e30b0df51c23175a257e248d8a162be88dd5c
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-06-28 01:15:41 +0200

    Add option to test without connecting to OEP API

--- app_settings.py
+++ app_settings.py
@@ -1,22 +1,24 @@
-# create sqlalchemy con
+
 import os
 from sqlalchemy import create_engine
 import sqlahelper
-
+import oedialect  # noqa pylint: disable=unused-import
 
 from wam import settings  # pylint: disable=import-error
-
 
 # add the package leaflet to the wam core project installed apps
 if os.name == 'posix':
     settings.INSTALLED_APPS.append('leaflet')
-LOCAL_TESTING = True
+
+LOCAL_TESTING = settings.config['WAM'].as_bool('LOCAL_TESTING')
 wam_config = settings.config['DATABASES']['OEP']
+wam_config = settings.config['DATABASES']['OEP_DIALECT']
 
 # ##########################################SQLAlchemy ENGINE#######################################
-# db connection string for sqlalchemy-engine
-DB_URL = '{ENGINE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**wam_config)
-
-engine = create_engine(DB_URL)
-sqlahelper.add_engine(engine, 'oep_engine')
+if not LOCAL_TESTING:
+    # db connection string for sqlalchemy-engine
+    # DB_URL = '{ENGINE}://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}'.format(**wam_config)
+    DB_URL = '{ENGINE}://{USER}:{TOKEN}@{HOST}'.format(**wam_config)
+    engine = create_engine(DB_URL)
+    sqlahelper.add_engine(engine, 'oep_engine')
 # ##################################################################################################
