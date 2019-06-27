62d2ed933383b0699d798bfc3522aed37bec7767
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-06-28 01:21:48 +0200

    Remove the models and urls

--- urls.py
+++ urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from django.conf.urls import url
-from djgeojson.views import GeoJSONLayerView
 from WAM_APP_FRED.serial_views import Serializer
 from . import views
-from .models import WeatherStation
-
 
 app_name = 'WAM_APP_FRED'
 
@@ -15,5 +11,4 @@ urlpatterns = [
     # just point geometrys as geojson featureCollection
     path('Locations.data/', Serializer.wseries_geometry_view, name='Locations.data'),
     path('MouseClick.data/', Serializer.wseries_get_single_point, name='MouseClick.data'),
-    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=WeatherStation), name='data')
 ]
