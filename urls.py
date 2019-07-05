98a9cb2de14390496ef8f746a6c834bd424627d2
Jonas H <jonas.huber@rl-institut.de>
2019-07-05 23:19:32 +0200

    new urls for powerplants and popupcontend

--- urls.py
+++ urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from WAM_APP_FRED.serial_views import (
     Serializer,
     wseries_get_single_point,
-    wseries_fetch_data_single_point
+    wseries_fetch_data_single_point,
+    ppr_popup_view
 )
 from . import views
 
@@ -12,7 +13,7 @@ urlpatterns = [
     # path('', views.fred_map, name='index')
     path('', views.webgui_test, name='index'),
     # just point geometrys as geojson featureCollection
-    path('Locations.data/', Serializer.wseries_geometry_view, name='Locations.data'),
+    # path('Locations.data/', Serializer.wseries_geometry_view, name='Locations.data'),
     path('MouseClick.data/', wseries_get_single_point, name='MouseClick.data'),
     path(
         'WeatherPointClick.data/',
@@ -20,5 +21,8 @@ urlpatterns = [
         name='WeatherPointClick.data'
     ),
     path('GerBoundary.data/', Serializer.ger_boundaries_view, name='GerBoundary.data'),
-    path('PowerPlantRegister.data/', Serializer.ppr_view, name='PowerPlantRegister.data')
+    
+    path('PowerPlantRegister.data/', Serializer.ppr_view, name='PowerPlantRegister.data'),
+    path('PowerPlantPopup.data/', ppr_popup_view, name='PowerPlantPopup.data')
+
 ]
