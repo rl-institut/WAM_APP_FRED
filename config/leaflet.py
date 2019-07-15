01f940adf37ec4aecd1c50aa4ac8a405e6cfbe6b
Jonas H <jonas.huber@rl-institut.de>
2019-07-15 15:30:09 +0200

    Fix tiles not loading when zooming out, adjusted initial zoom level, min zoom level value should not change when changing the behavior is unexpected

--- config/leaflet.py
+++ config/leaflet.py
@@ -8,29 +8,22 @@ LEAFLET_CONFIG = {
     'TILES': [('Streets', 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
                {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
                 'id': 'mapbox.streets',
-                'maxZoom': 100,
-                'minZoom': 9,
-                }
-               ),
+                }),
               ('OSM B&W', 'http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png',
                {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-                'minZoom': 9,
-                }
-               ),
+               }),
               ('OSM', 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                {'attribution': '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-                'minZoom': 9,
                 }),
               ('OpenTopoMap', 'http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
                {'attribution': 'Data: &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Display: &copy <a href="http://opentopomap.org">OpenTopoMap</a> <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-                'minZoom': 9,
                 })
               ],
-    # Germany center (center = long/2 and lat/2)
-    'DEFAULT_CENTER': (53.4554, 9.6211),
-    'DEFAULT_ZOOM': 10,
-    'MIN_ZOOM': 3,
-    'MAX_ZOOM': 1000,
+
+    'DEFAULT_CENTER': (51.9481, 10.2651),
+    'DEFAULT_ZOOM': 0,
+    'MAX_ZOOM': 40,
+    'MIN_ZOOM': 6,
     'RESET_VIEW': False,
     'NO_GLOBALS': False,
 }
