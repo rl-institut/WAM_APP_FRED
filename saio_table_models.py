c348c7344f4e978963fb649ae52d94e07048c2ed
christian-rli <christian.hofmann@rl-institut.de>
2019-07-29 16:28:04 +0200

    Reference complete feedin table #115

--- saio_table_models.py
+++ saio_table_models.py
@@ -2,5 +2,5 @@ import sqlahelper as sah
 import saio
 
 saio.register_schema("model_draft", sah.get_engine('oep_engine'))
-from saio.model_draft import openfred_timeseries_wind_2016 as Timeseries
+from saio.model_draft import openfred_feedin_wind_2016 as Timeseries
 from saio.model_draft import openfred_powerplants as Powerplants
