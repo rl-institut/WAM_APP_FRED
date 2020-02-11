aab3328de628c290b12ea50088a71a724dac7962
jh-RLI <jonas.huber@rl-institut.de>
2020-02-11 14:41:32 +0100

    change accesed schema

--- saio_table_models.py
+++ saio_table_models.py
@@ -1,6 +1,6 @@
 import sqlahelper as sah
 import saio
 
-saio.register_schema("model_draft", sah.get_engine('oep_engine'))
-from saio.model_draft import openfred_feedin_wind_2016 as Timeseries
-from saio.model_draft import openfred_powerplants as Powerplants
+saio.register_schema("supply", sah.get_engine('oep_engine'))
+from saio.supply import openfred_feedin_wind_2016 as Timeseries
+from saio.supply import openfred_powerplants as Powerplants
